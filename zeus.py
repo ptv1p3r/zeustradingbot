3624a5645db82459c0fda389a68f536527e8ae2d
v1p3r <pedro.roldan@gmail.com>
2018-01-21 17:28:01 +0000

    .

--- zeus.py
+++ zeus.py
@@ -5,12 +5,12 @@ import time
 
 def main():
 
-    chart = ZeusChart('ETH-EUR', 10)
+    chart = ZeusChart('ETH-EUR', 10) # inicia recolha na gdax
 
-    strategy = ZeusStrategy()
+    strategy = ZeusStrategy() # inicia estrategia
 
     candlesticks = []
-    developingCandlestick = ZeusCandlestick()
+    developingCandlestick = ZeusCandlestick() # inicia candlestick de desenvolvimento
 
     while True:
         developingCandlestick.tick(chart.getCurrentPrice())
@@ -20,7 +20,7 @@ def main():
             strategy.tick(developingCandlestick)
             developingCandlestick = ZeusCandlestick()
 
-        time.sleep(int(10))
+        time.sleep(int(10)) # espera 10 segundos
 
 
 if __name__ == "__main__":
