c3d4fd1fa91ca6adc782b22f82ceccb235246423
Pedro Roldan <pedro.roldan@gmail.com>
2018-01-21 15:57:16 +0000

    .

--- zeusstrategy.py
+++ zeusstrategy.py
@@ -12,7 +12,6 @@ class ZeusStrategy(object):
         self.currentPrice = ""
         self.currentClose = ""
         self.numTrades = 1
-
         self.indicators = ZeusIndicators()
 
     def tick(self, candlestick):
@@ -30,23 +29,29 @@ class ZeusStrategy(object):
 
     def evaluatePositions(self):
         openTrades = []
+
+        # verifica numero de trades abertas
         for trade in self.trades:
             if trade.status == "OPEN":
                     openTrades.append(trade)
 
+        # se o numero de posicoes abertas for inferior ao numero de trades permitidas
         if len(openTrades) < self.numTrades:
-            if self.currentPrice < self.indicators.movingAverage(self.prices, 15):
-                self.trades.append(ZeusTrade(self.currentPrice, stopLoss=.0001))
+            if self.currentPrice < self.indicators.movingAverage(self.prices, 15):  # se o preco corrente for inferior ao valor da media movel
+                self.trades.append(ZeusTrade(self.currentPrice, stopLoss=.0001))  # abre nova posicao
 
+        # verifica as posicoes abertas para fecho
         for trade in openTrades:
-            if self.currentPrice > self.indicators.movingAverage(self.prices, 15):
-                trade.close(self.currentPrice)
+            if self.currentPrice > self.indicators.movingAverage(self.prices, 15):  # se o preco corrente for inferior ao valor da media movel
+                trade.close(self.currentPrice)  # fecha posicao
 
+    # actualiza posicoes abertas
     def updateOpenTrades(self):
         for trade in self.trades:
             if trade.status == "OPEN":
                 trade.tick(self.currentPrice)
 
+    # mostra posicoes
     def showPositions(self):
         for trade in self.trades:
             trade.showTrade()
