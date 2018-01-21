bcf342aa6ceba727d7bee72ddb38a7bf74efe8d1
Pedro Roldan <pedro.roldan@gmail.com>
2018-01-21 16:28:29 +0000

    .

--- zeusindicators.py
+++ zeusindicators.py
@@ -1,7 +1,11 @@
+import numpy
+
+
 class ZeusIndicators(object):
     def __init__(self):
         pass
 
+    # media movel basica
     def movingAverage(self, dataPoints, period):
         if len(dataPoints) > 1:
             return sum(dataPoints[-period:]) / float(len(dataPoints[-period:]))
@@ -9,3 +13,18 @@ class ZeusIndicators(object):
     def momentum(self, dataPoints, period=14):
         if len(dataPoints) > period -1:
             return dataPoints[-1] * 100 / dataPoints[-period]
+
+    def EMA(self, prices, period):
+        x = numpy.asarray(prices)
+        weights = None
+        weights = numpy.exp(numpy.linspace(-1., 0., period))
+        weights /= weights.sum()
+
+        a = numpy.convolve(x, weights, mode='full')[:len(x)]
+        a[:period] = a[period]
+        return a
+
+    def MACD(self, prices, nslow=26, nfast=12):
+        emaslow = self.EMA(prices, nslow)
+        emafast = self.EMA(prices, nfast)
+        return emaslow, emafast, emafast - emaslow
